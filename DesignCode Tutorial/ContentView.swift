33f21989c848948932ddd514f09f40a3a6efead7
Tad Scritchfield <tscritchfield@demark.com>
2020-09-14 20:40:53 -0500

    Initial Commit

---
+++ DesignCode Tutorial/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DesignCode Tutorial
+//
+//  Created by Tad Scritchfield on 9/14/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DesignCode Tutorial/DesignCode_TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCode_TutorialApp.swift
+//  DesignCode Tutorial
+//
+//  Created by Tad Scritchfield on 9/14/20.
+//
+
+import SwiftUI
+
+@main
+struct DesignCode_TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
